97bac6130d5d372452ce2bc93c14f56de943d862
John Roque Jorillo <john.jorillo@appetiser.com.au>
2023-06-01 10:08:42 +0800

    Add description to maxSubArray

--- DSAPractice.playground/Sources/LeetCode/Data Structure/Array/Day1/MaxSubArray.swift
+++ DSAPractice.playground/Sources/LeetCode/Data Structure/Array/Day1/MaxSubArray.swift
@@ -4,6 +4,10 @@ public class Solution_maxSubArray {
     
     public init() {}
     
+    /*
+     Time: O(n) Since we loop only 1 time
+     Space: O(1) so other data structure created
+     */
     public func maxSubArray(_ nums: [Int]) -> Int {
         var sum = 0
         var ans = nums[0]
